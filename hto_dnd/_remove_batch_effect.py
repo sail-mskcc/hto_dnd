15ff24a575531c89d1f2cce28e56d89eb85eedf4
tobiaspk <tobiaspk1@gmail.com>
2025-01-10 17:43:27 -0500

    Implement support vector regression for denoising

--- hto_dnd/_remove_batch_effect.py
+++ hto_dnd/_remove_batch_effect.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import LinearSVR
 
 from ._defaults import DEFAULTS, DESCRIPTIONS
 from ._utils import get_arg
@@ -9,12 +10,22 @@ def remove_batch_effect(
     denoise_version: str,
     **kwargs,
 ):
-    print(denoise_version)
+    f"""
+    Remove batch effect of adt count data (x) using a cell-by-cell noise vector (covariates).
+
+    - v1: Linear regression of x on covariates and design matrix.
+    - v2: Support Vector Regression (SVR) of x on covariates.
+    """
     if denoise_version == "v1":
         return remove_batch_effect_v1(
             x=kwargs["x"],
             covariates=kwargs["covariates"],
             design=get_arg("design", kwargs, DEFAULTS),
+        )
+    elif denoise_version == "v2":
+        return remove_batch_effect_v2(
+            x=kwargs["x"],
+            covariates=kwargs["covariates"],
         )
     else:
         raise ValueError(f"Invalid version: {denoise_version}. Must be 'v1'.")
@@ -56,6 +67,39 @@ def remove_batch_effect_v1(
     # Store metadata
     meta = {
         "coefs": model.coef_,
+    }
+
+    return x_corrected, meta
+
+
+def remove_batch_effect_v2(
+    x,
+    covariates,
+    **kwargs_denoise,
+) -> np.ndarray:
+
+    # assertions
+    assert isinstance(x, np.ndarray), "Input matrix must be a NumPy array"
+    assert isinstance(covariates, np.ndarray), "Covariates must be a NumPy array"
+    covariates = np.asarray(covariates).reshape(-1, 1)
+
+    coefs = []
+    x_corrected = np.zeros_like(x)
+    for i in range(x.shape[1]):
+        x_i = x[:, i]
+
+        # Fit SVR model
+        model = LinearSVR(epsilon=0, fit_intercept=True, **kwargs_denoise)
+        model.fit(covariates, x_i)
+        coefs.append([model.intercept_, model.coef_[0]])
+
+        # apply correction
+        pred = model.predict(covariates)
+        x_corrected[:, i] = x_i - pred
+
+    # Store metadata
+    meta = {
+        "coefs": coefs,
     }
 
     return x_corrected, meta

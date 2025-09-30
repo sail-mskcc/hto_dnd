77bc911b2a3171bae3c5e6ca497325da699234b7
tobiaspk <tobiaspk1@gmail.com>
2025-09-30 15:28:07 -0400

    Debug case sensitivity in metrics

--- hto/metrics.py
+++ hto/metrics.py
@@ -26,6 +26,7 @@ def singlet_precision(
     # ignore case
     if ignore_case:
         prediction = prediction.str.lower()
+        ground_truth = ground_truth.str.lower()
         non_singlet_substring = [sub.lower() for sub in non_singlet_substring]
 
     # subset predicted singlets
